0716a62f8f8ee447cb91ddab7d2daa568f1d3314
InnoSufiyan <innosufiyan@gmail.com>
2023-08-11 19:33:01 +0500

    practice project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo-app';
+  todos: any[] = [];
+  addTask(task: any) {
+    this.todos.push({
+      id: this.todos.length,
+      name: task.value,
+    });
+    console.log(this.todos, '==>>todos');
+  }
+
+  removeTodo(id: number) {
+    console.log(id, '==>>id');
+    this.todos = this.todos.filter((todo) => todo.id != id);
+  }
 }
